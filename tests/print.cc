91c070e14e5fb11ac335ad888f74435873a94771
Arthus Leroy <arthus.leroy@epita.fr>
2019-11-25 15:55:10 +0100

    [TEST] print.cc: test print builtin function with arguments
    This tests the functions "import", "tuple", "dict", "builtins" and "call"

---
+++ tests/print.cc
@@ -0,0 +1,13 @@
+// TEST: import, tuple (complex form), dict (complex form), builtins, call
+
+# include "python.hh"
+
+int main()
+{
+    auto sys = Python::import("sys");
+
+    auto args = Python::tuple("this", "is", "a", "normal", "output");
+    auto kwargs = Python::dict("file", sys["stderr"], "sep", ",", "end", "\nhere\nis\nthe\nend\n");
+
+    Python::call_builtin("print", args, kwargs);
+}
