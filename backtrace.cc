8afb6d29f7dcad9483399d3b227d0d8e30914af8
Arthus Leroy <arthus.leroy@epita.fr>
2019-11-25 16:16:30 +0100

    [TRACE] backtrace.cc: add license header

--- backtrace.cc
+++ backtrace.cc
@@ -1,3 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2019, Arthus Leroy <arthus.leroy@epita.fr>
+ * On https://github.com/arthus-leroy/Python
+ * All rights reserved. */
+
 // WARNING: need to use "sudo chmod +s /usr/bin/gdb" to allow gdb capture here
 // Look at Ubuntu Wiki - Kernel hardening
 
