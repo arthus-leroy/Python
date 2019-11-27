987aa490c7a1ba59b4fcb805f52bb018bab2e2b6
Arthus Leroy <arthus.leroy@epita.fr>
2019-11-27 03:37:22 +0100

    [TEST] size.cc, constructors.cc: add tests

---
+++ tests/constructors.cc
@@ -0,0 +1,20 @@
+# include "python.hh"
+
+int main()
+{
+    auto sys = Python::import("sys");
+
+    Python(std::string("This should work, nah ?"));
+    Python("Don't you dare segfault on me");
+    Python(std::filesystem::path("/perfectly/valid/path_or_not"));
+    Python(0.001f);
+    Python(0.001);
+    Python(0.001l);
+    Python(42ull);
+    Python(42);
+    Python(42l);
+    Python(42ll);
+    // Python(sys["stderr"]) fails to compile miserably
+    Python{sys["stderr"]};
+    Python(true);
+}
