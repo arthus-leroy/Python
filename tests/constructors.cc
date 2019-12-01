2b1e3423fb7878e7776db261a0b9c578a69c77c7
Arthus Leroy <arthus.leroy@epita.fr>
2019-12-02 00:54:31 +0100

    [TEST] constructors.cc: add containers to test

--- tests/constructors.cc
+++ tests/constructors.cc
@@ -15,8 +15,25 @@ int main()
     Python(42);
     Python(42l);
     Python(42ll);
-    // FIXME: Python(sys["stderr"]) fails to compile miserably
-    Python{sys["stderr"]};
+
+    // True, False and None are not constructed, but shared
     Python(true);
-//    Python({1, 2, 3, 4, 5, 6});
+
+    Python({1, 2, 3, 4, 5, 6});
+
+    // the (void) are required, otherwise it's considered as a variable declaration
+    (void) Python(sys["stderr"]);
+
+    std::vector<double> a = {5, 9, 7, 2, 4};
+    (void) Python(a);
+
+    std::array<int, 10> b = {1, 9, 6, 7, 4, 2};
+    (void) Python(b);
+
+    std::map<std::string, unsigned> c = {{"key1", 1},
+                                         {"key2", 2},
+                                         {"key3", 3},
+                                         {"key4", 4}};
+
+    (void) Python(c);
 }
