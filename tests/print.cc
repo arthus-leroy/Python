dc5d298ac73579a8e5fc3fb01cdbf6cdf197044a
Arthus Leroy <arthus.leroy@epita.fr>
2019-12-10 17:52:08 +0100

    [TEST] print.cc: clearer kwargs repartition

--- tests/print.cc
+++ tests/print.cc
@@ -7,7 +7,9 @@ int main()
     auto sys = Python::import("sys");
 
     auto args = Python::tuple("this", "is", "a", "normal", "output");
-    auto kwargs = Python::dict("file", sys["stderr"], "sep", ",", "end", "\nhere\nis\nthe\nend\n");
+    auto kwargs = Python::dict("file", sys["stderr"],
+                               "sep" , ",",
+                               "end" , "\nhere\nis\nthe\nend\n");
 
     Python::call_builtin("print", args, kwargs);
 }
